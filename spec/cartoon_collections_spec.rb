d09b84cfe6b40bbd6a30b0bc8a841c7f70f1e6b4
classy-waterfall-6294 <silviamendoza223@yahoo.com>
2019-05-25 01:48:59 +0000

    Automatically backed up by Learn

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,41 +1,4 @@
-require "spec_helper"
 
-describe "Cartoon Collections" do
-  describe "#roll_call_dwarves" do
-    it "prints out the 7 dwarfs in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-      output = capture_stdout do
-      
-      expect(output).to match(/3.*Bashful/)
-    end
-  end
-
-  describe "#summon_captain_planet" do
-    it "returns an array with the same number of elements that it was given" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      expect(result.class).to eq(Array)
-      expect(result.length).to eq(3)
-    end
-
-    it "capitalizes each element and adds an exclamation mark (test 1)" do
-      fruits = ["apple", "banana", "orange"]
-      result = summon_captain_planet(fruits)
-      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
-    end
-
-    it "capitalizes each element and adds an exclamation mark (test 2)" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
-    end
-  end
-
-  describe "#long_planeteer_calls" do
-    it "returns true if any calls are longer than 4 characters" do
-      calls_long = ["axe", "earth", "wind", "fire"]
-      expect(long_planeteer_calls(calls_long)).to eq(true)
     end
 
     it "returns false if all calls are 4 characters or less" do
